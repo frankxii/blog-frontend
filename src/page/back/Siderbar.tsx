bf7bc434b93fca6726d9e0995d09217473a7b12e
frank <frankw.wang@qq.com>
2021-04-18 19:29:04 +0800

    修改 优化后台侧边栏高亮展示

--- src/page/back/Siderbar.tsx
+++ src/page/back/Siderbar.tsx
@@ -1,21 +1,26 @@
-import React from "react"
+import React, {useState} from "react"
 import {Menu} from "antd"
 import {Link} from "react-router-dom"
 
 const {SubMenu} = Menu
 
 export default function Siderbar(props: any) {
+  const [selectedKey, setSelectedKey] = useState('')
+  
   return (
     <Menu
       style={{width: 208, height: "100vh"}}
       defaultOpenKeys={["articles"]}
+      selectedKeys={[selectedKey]}
       mode="inline"
+      // 切换高亮tab
+      onClick={event => setSelectedKey(event.key.toString())}
     >
       <SubMenu key="articles" title="文章编辑">
         <Menu.Item key="articleList">
           <Link to={`${props.match.path}/article`}>文章列表</Link>
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key='category'>
           <Link to={`${props.match.path}/category`}>分类管理</Link>
         </Menu.Item>
       </SubMenu>
