182c6798f369b1801895225adaf2790dd70a0e40
frank <frankw.wang@qq.com>
2021-04-18 23:12:31 +0800

    新增 分类新增/编辑/删除功能，使用useState, useRef

--- src/page/back/Category.tsx
+++ src/page/back/Category.tsx
@@ -1,14 +1,21 @@
-import React, {useEffect, useState} from "react"
-// import {Link} from "react-router-dom"
-import {Table, Button, Space, Modal, Input} from "antd"
+import React, {useEffect, useRef, useState} from "react"
+import {Table, Button, Space, Modal, Input, message} from "antd"
 import axios from "axios"
 
 export default function Category() {
-  const [categories, setCategories] = useState([{id: 0, name: ''}])
+  // 分类列表数据
+  const [categories, setCategories] = useState([{id: null, name: ''}])
+  // 弹窗展示bool值
   const [isModalVisible, setIsModalVisible] = useState(false)
-  // const [modalTitle, setModalTitle] = useState('新增分类')
+  // 弹窗内部属性，type:操作类型，id:类别id，name：类别名称
   const [modalProps, setModalProps] = useState({type: 'add', id: 0, name: ''})
-  const [inputValue, setInputValue] = useState('')
+  // 弹窗确定按钮loading bool值
+  const [confirmLoading, setConfirmLoading] = useState(false)
+  // 提交成功后刷新列表
+  const [refresh, setRefresh] = useState(0)
+  // 弹窗input组件引用
+  const inputRef = useRef(null)
+  // 表格列结构
   const columns = [
     {
       key: "id",
@@ -27,17 +34,17 @@ export default function Category() {
       width: 200,
       render: renderOperation
     }
-
   ]
 
+  // 表格内操作区域的渲染函数
   function renderOperation(record: any) {
-    if (record && record.id !== 0) {
+    if (record.id) {
       return (
         <Space>
           <Button
             type="primary"
             size={"small"}
-            onClick={() => handleDelete(record)}
+            onClick={() => showEditModal(record)}
           >编辑
           </Button>
           <Button
@@ -52,17 +59,68 @@ export default function Category() {
     }
   }
 
-  function handleDelete(record: any) {
+  function showEditModal(record: any) {
     setModalProps({type: 'edit', id: record.id, name: record.name})
     setIsModalVisible(true)
   }
 
-
-  function handleOk(event: any) {
-    console.log(modalProps.id, modalProps.type)
-    console.log(inputValue)
+  // 弹窗确定-新增/编辑 处理回调
+  function handleOk() {
+    setConfirmLoading(true)
+    let isAdd = modalProps.type === 'add'
+    // 如果then抛出异常，则catch不处理
+    let isMessageShowed: boolean = false
+    // @ts-ignore
+    let data = {id: modalProps.id, name: inputRef.current.state.value}
+    axios({
+      method: isAdd ? 'post' : 'put',
+      url: '/blog/category',
+      data: data,
+    })
+      .then(response => {
+        if (response.data.ret === 0) {
+          setIsModalVisible(false)
+          message.success(`${isAdd ? '新增' : '更新'}分类成功`,).then()
+          setRefresh(refresh + 1)
+        } else {
+          message.error(response.data.msg,).then()
+        }
+        isMessageShowed = true
+      })
+      .catch(() => {
+        if (!isMessageShowed) {
+          message.error('网络异常').then()
+        }
+      })
+      .finally(() => {
+          setConfirmLoading(false)
+        }
+      )
   }
 
+  // 删除分类
+  function handleDelete(record: any) {
+    console.log(record)
+    let isMessageShowed: boolean = false
+    axios
+      .delete('/blog/category', {data: {id: record.id}})
+      .then(r => {
+        if (r.data.ret === 0) {
+          message.success('删除成功').then()
+          setRefresh(refresh + 1)
+        } else {
+          message.error('删除失败').then()
+        }
+        isMessageShowed = true
+      })
+      .catch(() => {
+        if (!isMessageShowed) {
+          message.error('网络异常').then()
+        }
+      })
+  }
+
+  // 刷新表格
   useEffect(() => {
     axios.get('/blog/categoryList')
       .then(r => {
@@ -70,7 +128,15 @@ export default function Category() {
           setCategories(r.data.data)
         }
       })
-  }, [])
+  }, [refresh])
+
+  // 每次弹窗关开启清理input的值
+  useEffect(() => {
+    if (isModalVisible) {
+      // @ts-ignore
+      inputRef.current.setState({value: ''})
+    }
+  }, [isModalVisible])
 
   return (
     <div>
@@ -91,11 +157,13 @@ export default function Category() {
         visible={isModalVisible}
         onOk={handleOk}
         onCancel={() => setIsModalVisible(false)}
+        confirmLoading={confirmLoading}
       >
         <Input
           addonBefore='分类名称'
           allowClear={true}
-          onChange={event => setInputValue(event.target.value)}
+          ref={inputRef}
+          onPressEnter={handleOk}
         />
       </Modal>
     </div>
