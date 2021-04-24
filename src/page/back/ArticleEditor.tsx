4b87881f54b80e44696f8e47dd6e02b6875cad0b
frank <frankw.wang@qq.com>
2021-04-24 19:26:27 +0800

    新增 新增、修改文章支持选择分类

--- src/page/back/ArticleEditor.tsx
+++ src/page/back/ArticleEditor.tsx
@@ -1,37 +1,62 @@
 import React, {useEffect, useRef, useState} from "react"
 import MDEditor from '@uiw/react-md-editor'
-import {Input, Divider, Button, message} from "antd"
+import {Input, Divider, Button, message, Select} from "antd"
 import {Row, Col} from "antd"
 import axios from "axios"
 
 export default function ArticleEditor(props: any) {
 
+  // 文章内容
   const [value, setValue] = useState('')
+  // 文章id
   const [id, setId] = useState(0)
+  // 标题输入框引用
   const inputRef = useRef(null)
+  // 分类下拉列表
+  const [categories, setCategories] = useState([{id: 0, name: ''}])
+  // 当前选择分类
+  const [category, setCategory] = useState({id: 0, name: ''})
+  const {Option} = Select
 
-  useEffect(() => {
+  // 填充编辑器
+  useEffect(function constructor() {
     let id = props.match.params.id
     // 如果路由带id，说明是编辑，请求文章详情填充编辑器
     if (id !== undefined) {
       axios.get('/blog/article', {params: {id: id}})
         .then(r => {
           if (r.data.ret === 0) {
-            setValue(r.data.data.body)
+            let article = r.data.data
+            setValue(article.body)
             // @ts-ignore
-            inputRef.current.state.value = r.data.data.title
+            inputRef.current.state.value = article.title
             setId(id)
+            setCategory({id: article.category_id, name: article.category_name})
           }
         })
     } else {
       // 清空标题和内容，主要应用场景为编辑后跳转新建页面
       setValue('')
+      setCategory({id: 0, name: ''})
       // @ts-ignore
       inputRef.current.setValue('')
     }
   }, [props])
+  // 获取分类
+  useEffect(function getCategories() {
+    axios.get('/blog/categoryList')
+      .then(response => {
+        if (response.data.ret === 0) {
+          let categories = response.data.data
+          setCategories(categories)
+          setCategory(categories[0])
+        }
+      })
+
+  }, [])
 
   function saveArticle() {
+    console.log(category)
     if (inputRef) {
       // @ts-ignore
       const title = inputRef.current.state.value
@@ -43,7 +68,7 @@ export default function ArticleEditor(props: any) {
       axios({
         url: '/blog/article',
         method: id === 0 ? 'post' : 'put',
-        data: {id: id, title: title, body: value}
+        data: {id: id, title: title, body: value, category_id: category.id}
       })
         .then(r => {
           if (r.data.ret === 0) {
@@ -59,11 +84,25 @@ export default function ArticleEditor(props: any) {
     }
   }
 
+
   return (
     <div>
       <Row gutter={20}>
         <Col span={12}>
           <Input ref={inputRef} addonBefore="标题"/>
+        </Col>
+        <Col>
+          <Select
+            // @ts-ignore
+            value={category.name}
+            style={{width: 120}}
+            // 修改分类id
+            onChange={(value: string, option: any) => setCategory({id: parseInt(option.key), name: value})}
+          >
+            {categories.map((category) => (
+              <Option key={category.id} value={category.name}>{category.name}</Option>
+            ))}
+          </Select>
         </Col>
         <Col>
           <Button type={"primary"} onClick={saveArticle}>保存</Button>
