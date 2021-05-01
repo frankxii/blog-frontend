9afe90cbfa05eeaf60c92554d6c2e89095ab00d0
frank <frankw.wang@qq.com>
2021-05-01 20:48:50 +0800

    修改 分类标签由假数据改为请求后端接口

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,21 @@
 export const api = {
 
+  // 文章
   getArticle: ['/blog/article', 'get'],
   addArticle: ['/blog/article', 'post'],
   updateArticle: ['/blog/article', 'put'],
   deleteArticle: ['/blog/article', 'delete'],
 
+  //文章列表
   getArticleList: ['/blog/articleList', 'get'],
   getCategoryList: ['/blog/categoryList', 'get'],
 
+  //分类
   getCategory: ['/blog/category', 'get'],
   addCategory: ['/blog/category', 'post'],
   updateCategory: ['/blog/category', 'put'],
   deleteCategory: ['/blog/category', 'delete'],
 
+  //归档
+  getArchive: ['/blog/archive', 'get']
 }
--- src/component/CategoryPendant.tsx
+++ src/component/CategoryPendant.tsx
@@ -1,27 +1,59 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Card, Divider} from 'antd'
 import {Link} from "react-router-dom"
+import request from "../request"
+import {api} from '../api'
 
 export default function CategoryPendant(props: any) {
+  const [categories, setCategories] = useState([{name: '', count: 0}])
+  const roman_numbers = ['Ⅰ', 'Ⅱ', 'Ⅲ', 'Ⅳ', 'Ⅴ', 'Ⅵ', 'Ⅶ', 'Ⅷ', 'Ⅸ', 'Ⅹ']
+  let index = 0
 
-  return (
-    <Card
-      style={{width: 200, borderRadius: 8}}
-    >
-      <p style={{textAlign: 'center'}}>分类</p>
-      <Divider/>
-      <Link to='/blog'>
-        <p>Ⅰ 生活随笔~~~~~7</p>
-      </Link>
-      <Link to='/blog'>
-        <p>Ⅱ 生活随笔~~~~~8</p>
-      </Link>
-      <Link to='/blog'>
-        <p>Ⅳ 生活~~~~~~~~6</p>
-      </Link>
-      <Link to='/blog'>
-        <p>Ⅲ 生活随笔~~~~~9</p>
-      </Link>
-    </Card>
-  )
+  useEffect(() => {
+    request(api.getArchive, {cate: 'category'})
+      .then(res => {
+        setCategories(res.data)
+      })
+  }, [])
+
+  function getCategoryText(cate_name: string, count: number) {
+    // 获取对应罗马数字
+    let roman_number = roman_numbers[index]
+    // 中文四位时~符号展示5次，随中文字符长度增减，一个中文补位需要两个~符号
+    let character_total = 5
+    character_total -= (cate_name.length - 4) * 1.5
+    character_total = Math.floor(character_total)
+    // 如果count多一位，字符就少一位
+    character_total -= count.toString().length - 1
+    // 为了排版，奇数汉字时加入一个1/3空格
+    // https://segmentfault.com/a/1190000022353208
+    let fill = ''
+    if (cate_name.length % 2 !== 0) {
+      fill = '\u2004'
+    }
+    let character = new Array(character_total + 1).join('~')
+    // 生成一条分类text后，罗马数字索引+1
+    index += 1
+    return roman_number + ' ' + cate_name + character + fill + count.toString()
+  }
+
+  if (categories[0].name !== '') {
+    return (
+      <Card
+        style={{width: 200, borderRadius: 8}}
+      >
+        <p style={{textAlign: 'center'}}>分类</p>
+        <Divider/>
+        {categories.map(category => {
+          return (
+            <Link to={`/blog/category/${category.name}`} key={category.name}>
+              <p>{getCategoryText(category.name, category.count)}</p>
+            </Link>
+          )
+        })}
+      </Card>
+    )
+  } else {
+    return (<span/>)
+  }
 }
