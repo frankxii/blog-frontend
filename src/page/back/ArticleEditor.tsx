7b30c68357fed4a094251bba8bea267242ffcbbd
frank <frankw.wang@qq.com>
2021-05-03 03:57:59 +0800

    新增 编辑器增加标签功能

--- src/page/back/ArticleEditor.tsx
+++ src/page/back/ArticleEditor.tsx
@@ -5,6 +5,7 @@ import {Row, Col} from "antd"
 import request from "../../request"
 import {api} from "../../api"
 
+
 export default function ArticleEditor(props: any) {
 
   // 文章内容
@@ -16,7 +17,11 @@ export default function ArticleEditor(props: any) {
   // 分类下拉列表
   const [categories, setCategories] = useState([{id: 0, name: ''}])
   // 当前选择分类
-  const [category, setCategory] = useState({id: 0, name: ''})
+  const [category, setCategory] = useState<{ id: number, name: string }>({id: 0, name: ''})
+
+  // 标签
+  const [tags, setTags] = useState<{ id: number, name: string }[]>([])
+  const [selectedTags, setSelectedTags] = useState<[string | number][]>([])
   const {Option} = Select
 
   // 填充编辑器
@@ -34,9 +39,11 @@ export default function ArticleEditor(props: any) {
           setCategory({id: article.category_id, name: article.category_name})
         })
     } else {
-      // 清空标题和内容，主要应用场景为编辑后跳转新建页面
+      // 清空标题、内容、分类、标签，主要应用场景为编辑后跳转新建页面
       setValue('')
       setCategory({id: 0, name: ''})
+      setTags([])
+      setSelectedTags([])
       // @ts-ignore
       inputRef.current.setValue('')
     }
@@ -50,17 +57,20 @@ export default function ArticleEditor(props: any) {
     })
   }, [])
 
+  // 新增或修改文章
   function saveArticle() {
+    console.log(selectedTags)
     if (inputRef) {
+      // 非空校验
       // @ts-ignore
       const title = inputRef.current.state.value
       if (title === undefined || value === '') {
         message.warning('标题和内容不得为空', 2).then()
         return
       }
-
+      // 创建或修改文章
       request(id === 0 ? api.addArticle : api.updateArticle,
-        {id: id, title: title, body: value, category_id: category.id}
+        {id: id, title: title, body: value, category_id: category.id, tags: selectedTags}
       ).then(res => {
         message.success(id === 0 ? '创建成功' : '保存成功', 2).then()
         if ('data' in res) {
@@ -70,11 +80,26 @@ export default function ArticleEditor(props: any) {
     }
   }
 
+  // 每次标签变化会回调这个方法
+  function tagOnChange(keys: [string | number][]) {
+    let keysChecked: [string | number][] = []
+    keys.forEach((key: any) => {
+      // 如果输入的标签输入已有标签，用已有标签的id替换name
+      if (typeof key === "string") {
+        tags.forEach((tag) => {
+          if (tag.name === key) key = tag.id
+        })
+      }
+      keysChecked.push(key)
+    })
+    setSelectedTags(keysChecked)
+  }
+
 
   return (
     <div>
       <Row gutter={20}>
-        <Col span={12}>
+        <Col span={8}>
           <Input ref={inputRef} addonBefore="标题"/>
         </Col>
         <Col>
@@ -88,6 +113,16 @@ export default function ArticleEditor(props: any) {
             {categories.map((category) => (
               <Option key={category.id} value={category.name}>{category.name}</Option>
             ))}
+          </Select>
+        </Col>
+        <Col span={5}>
+          <Select
+            mode="tags"
+            style={{width: '100%'}}
+            // @ts-ignore
+            onChange={tagOnChange}
+          >
+            {tags.map((tag) => (<Option value={tag.id}>{tag.name}</Option>))}
           </Select>
         </Col>
         <Col>
